f37dc68822b575347b6002cef3f084f5402d85e5
Olovuddin <olovuddinramiddinov26@gmail.com>
2024-12-06 06:00:42 +0500

    Product detail

---
+++ src/app/product/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="w-full h-screen bg-black">
+      <h1>Product Not Found</h1>
+      <p>Please try again</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
