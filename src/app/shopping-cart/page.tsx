23d6da54a7008e0dd0effa753a11ee6eec471476
Olovuddin <olovuddinramiddinov26@gmail.com>
2024-12-09 13:49:08 +0500

    save

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -177,7 +177,7 @@ const ShoppingCart = () => {
             </div>
             <h1 className="mt-5 text-[36px] font-bold text-slate-800 lg:text-[50px]">Shopping cart is empty</h1>
             <p className="text-slate-600 mt-5 lg:text-lg">
-              The page you are looking for doesn't exist or <br />
+              The page you are looking for doesn`t exist or <br />
               has been removed.
             </p>
             <Link href={"/products"}>
